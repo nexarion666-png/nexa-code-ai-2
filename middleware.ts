9e53576688d6de3a303b6e7e37e2ac3bbc562eaf
Nexa Dev <you@example.com>
2026-09-18 19:48:07 +0100

    Fix implicit any cookiesToSet

--- middleware.ts
+++ middleware.ts
@@ -25,7 +25,7 @@ export async function middleware(request: NextRequest) {
       getAll() {
         return request.cookies.getAll();
       },
-      setAll(cookiesToSet) {
+      setAll(cookiesToSet: any) {
         cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
         response = NextResponse.next({ request });
         cookiesToSet.forEach(({ name, value, options }) => response.cookies.set(name, value, options));
